14c7341441769f35c5b2262b9c17b1b1d071c2ce
raythurman2386 <raymondthurman5@gmail.com>
2024-04-09 21:36:54 -0500

    initial commit setting up kingdom knight game and files

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,31 +1,36 @@
-import { Boot } from './scenes/Boot';
-import { GameOver } from './scenes/GameOver';
-import { Game as MainGame } from './scenes/Game';
-import { MainMenu } from './scenes/MainMenu';
-import { AUTO, Game } from 'phaser';
-import { Preloader } from './scenes/Preloader';
+import { Game, Types } from "phaser";
+import { LoadingScene } from "./scenes";
 
-//  Find out more information about the Game Config at:
-//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
-const config: Phaser.Types.Core.GameConfig = {
-    type: AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        MainGame,
-        GameOver
-    ]
+const gameConfig: Types.Core.GameConfig = {
+    title: "Kingdom Knight",
+    type: Phaser.WEBGL,
+    parent: "game",
+    backgroundColor: "#351f1b",
+    scale: {
+        mode: Phaser.Scale.ScaleModes.NONE,
+        width: window.innerWidth,
+        height: window.innerHeight,
+    },
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false,
+        },
+    },
+    render: {
+        antialiasGL: false,
+        pixelArt: true,
+    },
+    canvasStyle: `display: block; width: 100%; height: 100%;`,
+    autoFocus: true,
+    audio: {
+        disableWebAudio: false,
+    },
+    scene: [LoadingScene],
 };
 
 const StartGame = (parent: string) => {
-
-    return new Game({ ...config, parent });
-
-}
+    return new Game({ ...gameConfig, parent });
+};
 
 export default StartGame;
